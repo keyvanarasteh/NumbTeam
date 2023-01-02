51dfc0c908b87683a6bfd0e642badfaf8d1c6d5b
mehmetjank <211216058@stu.istinye.edu.tr>
2023-01-02 20:49:51 +0300

    optimization and files organized

--- lib/responsive/screens/large/large_screen.dart
+++ lib/responsive/screens/large/large_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../class/desktop_and_large_appbar.dart';
 import '../../../utils/colors.dart';
 
 class LargeScreen extends StatelessWidget {
@@ -9,174 +10,14 @@ class LargeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColors.secondaryColor,
-        appBar: AppBar(
-          backgroundColor: AppColors.secondaryColor,
-          automaticallyImplyLeading: false,
-          leading: Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(20, 5, 0, 5),
-            child: Image.asset(
-              'assets/images/logo/github_logo.png',
-              // width: 250,
-              // height: 250,
-              // fit: BoxFit.cover,
-            ),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(50),
+          child: DesAndLarAppBar(
+            title: const Text('Desktop Screen'),
+            backgroundColor: AppColors.secondaryColor,
+            appBar: AppBar(),
+            actions: const <Widget>[],
           ),
-          title: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              //add small text field
-              ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: 250,
-                  maxHeight: 40,
-                ),
-                child: Expanded(
-                  child: Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
-                    child: TextFormField(
-                      controller: TextEditingController(),
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        labelText: 'Search or jump to...',
-                        labelStyle: const TextStyle(
-                          color: Color(0xFF8B949E),
-                          fontSize: 14,
-                        ),
-                        hintText: 'Search or jump to...',
-                        hintStyle: const TextStyle(
-                          color: Color(0xFF8B949E),
-                          fontSize: 14,
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Color(0xFF24292E),
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Color(0xFF24292E),
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        filled: true,
-                        fillColor: const Color(0xFF30363D),
-                      ),
-                      style: const TextStyle(
-                        color: Color(0xFFC9D1D9),
-                        fontSize: 14,
-                      ),
-                      keyboardType: TextInputType.text,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
-                child: TextButton(
-                  onPressed: () => print('Pull_request_button pressed ...'),
-                  child: const Text(
-                    'Pull requests',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
-                child: TextButton(
-                  onPressed: () => print('Issues_button pressed ...'),
-                  child: const Text(
-                    'Issues',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
-                child: TextButton(
-                  onPressed: () => print('Codespaces_button pressed ...'),
-                  child: const Text(
-                    'Codespaces',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
-                child: TextButton(
-                  onPressed: () => print('Marketplace_button pressed ...'),
-                  child: const Text(
-                    'Marketplace',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
-                child: TextButton(
-                  onPressed: () => print('Explore_button pressed ...'),
-                  child: const Text(
-                    'Explore',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          actions: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
-                  child: TextButton(
-                    onPressed: () => print('Notification_button pressed ...'),
-                    child: const Icon(
-                      Icons.notifications,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      size: 24,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
-                  child: TextButton(
-                    onPressed: () => print('Plus_button pressed ...'),
-                    child: const Icon(
-                      Icons.add,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      size: 24,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(5, 5, 10, 5),
-                  child: CircleAvatar(
-                    radius: 20,
-                    backgroundImage: const NetworkImage(
-                        'https://avatars.githubusercontent.com/u/47338871?v=4'),
-                  ),
-                ),
-              ],
-            ),
-          ],
         ),
         body: const Center(
           child: Text('Large Screen'),
