c9cb3984712bde4370be04c6864225d0b60e7a67
seckin111 <seckin2002gs@gmail.com>
2023-01-03 15:31:14 +0300

    DropDownButton color changed

--- lib/responsive/screens/large/large_screen.dart
+++ lib/responsive/screens/large/large_screen.dart
@@ -445,7 +445,7 @@ class _DropdownButtonExampleState extends State<DropdownButtonExample> {
       value: dropdownValue,
       icon: const Icon(Icons.arrow_downward),
       elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
+      style: const TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
       underline: Container(
         height: 2,
         color: Colors.deepPurpleAccent,
